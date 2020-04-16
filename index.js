721b2392c422a4b15f1ac623fe8183981bf4bdce
Balint Szebenyi <szebenyib@gmail.com>
2020-04-16 14:58:26 +0200

    Adding text control 03

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-
-], function() {
+  "sap/m/Text"
+], function(Text) {
   "use strict";
-  alert("UI5 is ready.");
+  new Text({
+    text: "Hello World"
+  }).placeAt("content");
 });
