a1aea4a61ebb8d7d6d87d8ddae526de45444aaa3
Balint Szebenyi <szebenyib@gmail.com>
2020-05-07 14:21:15 +0200

    Adding messagetoast 06

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function(Controller) {
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+], function(Controller,
+            MessageToast) {
   "use strict";
   return Controller.extend("", {
     onShowHello: function() {
-      alert("Hi");
+      MessageToast.show("Hi!");
     }
   });
 });
