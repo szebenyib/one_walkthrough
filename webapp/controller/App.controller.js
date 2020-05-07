cb16990c550700b406b5f8c58f8e18c6931732ef
Balint Szebenyi <szebenyib@gmail.com>
2020-05-07 15:29:48 +0200

    Adding jsonmodel 07

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,21 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
+  "sap/m/MessageToast",
+  "sap/ui/model/json/JSONModel"
 ], function(Controller,
-            MessageToast) {
+            MessageToast,
+            JSONModel) {
   "use strict";
   return Controller.extend("", {
+    onInit: function() {
+      var oData = {
+        recipient: {
+          name: "world"
+        }
+      };
+      var oModel = new JSONModel(oData);
+      this.getView().setModel(oModel);
+    },
     onShowHello: function() {
       MessageToast.show("Hi!");
     }
