19b8514ead4c6cc41986403fed57f61c2c57c525
Balint Szebenyi <szebenyib@gmail.com>
2020-05-07 16:32:39 +0200

    Adding i18n 08

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/m/MessageToast",
-  "sap/ui/model/json/JSONModel"
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/model/resource/ResourceModel"
 ], function(Controller,
             MessageToast,
-            JSONModel) {
+            JSONModel,
+            ResourceModel) {
   "use strict";
   return Controller.extend("", {
     onInit: function() {
@@ -15,9 +17,17 @@ sap.ui.define([
       };
       var oModel = new JSONModel(oData);
       this.getView().setModel(oModel);
+      var oI18nModel = new ResourceModel({
+        bundleName: "one_walkthrough.i18n.i18n"
+      });
+      this.getView().setModel(oI18nModel, "i18n");
     },
     onShowHello: function() {
-      MessageToast.show("Hi!");
+      var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+      var oModel = this.getView().getModel("i18n");
+      var oBundle = oModel.getResourceBundle();
+      var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+      MessageToast.show(sMsg);
     }
   });
 });
