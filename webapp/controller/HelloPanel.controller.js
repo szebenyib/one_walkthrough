e8a42efc7e7561ff3c79746f37553b814f2b6703
Balint Szebenyi <szebenyib@gmail.com>
2020-06-11 14:55:09 +0200

    Adding hello dialog fragment 16

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
+  "sap/m/MessageToast",
+  "sap/ui/core/Fragment"
 ], function(Controller,
-            MessageToast) {
+            MessageToast,
+            Fragment) {
   "use strict";
   return Controller.extend("one_walkthrough.controller.HelloPanel", {
     onShowHello: function() {
@@ -11,6 +13,20 @@ sap.ui.define([
       var oBundle = oModel.getResourceBundle();
       var sMsg = oBundle.getText("helloMsg", [sRecipient]);
       MessageToast.show(sMsg);
+    },
+    onOpenDialog: function() {
+      var oView = this.getView();
+      if (!this.byId("helloDialog")) {
+        Fragment.load({
+          id: oView.getId(),
+          name: "one_walkthrough.view.HelloDialog"
+        }).then(function (oDialog){
+          oView.addDependent(oDialog);
+          oDialog.open();
+        });
+      } else {
+        this.byId("helloDialog").open();
+      }
     }
   });
 });
