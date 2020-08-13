00f436bf778fb42afff91d1fc8ad23a8d53a4841
Kalamar Gyorgyi <gyorgyi.kalamar@onespire.hu>
2020-08-13 15:33:06 +0200

    Step 29

---
+++ webapp/test/integration/NavigationJourney.js
@@ -0,0 +1,30 @@
+/*global QUnit, opaTest*/
+
+sap.ui.define([
+  "one_walkthrough/localService/mockserver",
+  "sap/ui/test/opaQunit",
+  "./pages/App"
+], function (mockserver) {
+  "use strict";
+  QUnit.module("Navigation");
+  opaTest("Vajon megnyílik a Hello dialógus?", function (Given, When, Then) {
+		// initialize the mock server
+		mockserver.init();
+
+		// Arrangements
+		Given.iStartMyUIComponent({
+			componentConfig: {
+				name: "one_walkthrough"
+			}
+		});
+
+		//Actions
+		When.onTheAppPage.iPressTheSayHelloWithDialogButton();
+
+		// Assertions
+		Then.onTheAppPage.iShouldSeeTheHelloDialog();
+
+		// Cleanup
+		Then.iTeardownMyApp();
+	});
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,13 @@
+/* global QUnit */
+
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"one_walkthrough/test/integration/NavigationJourney"
+	], function () {
+		QUnit.start();
+	});
+});
